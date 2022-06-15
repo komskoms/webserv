61ad5f3a4623278721e51fc68887c5216eac066e
Liemani <parkjh2768@naver.com>
2022-06-16 06:06:36 +0900

    fix: fix a bug where response of GET for file could not send body

--- Response.cpp
+++ Response.cpp
@@ -27,11 +27,10 @@ void Response::appendMessage(const std::string& message) {
 //      clientSocket: The socket fd of client.
 //  - Returns: See the type definition.
 ReturnCaseOfSend Response::sendResponseMessage(int clientSocket) {
-    if (this->_sendBegin == NULL) {
+    if (this->_sendBegin == NULL)
         this->_sendBegin = &this->_message[0];
+    if (this->_messageDataSize = 0)
         this->_messageDataSize = this->_message.length();
-    }
-
 
     const std::string::size_type sendedSize = this->_sendBegin - &this->_message[0];
     const std::string::size_type sizeToSend = this->_messageDataSize - sendedSize;
